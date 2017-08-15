8e625a8288cfd932253d624a8055f84e291f1804
chex <summermick@gmail.com>
2017-08-15 10:42:32 -0700

    integrate fuse app to laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,49 @@
 <!doctype html>
-<html lang="en">
+<html ng-app="fuse">
 <head>
-    <meta charset="utf-8">
-    <title>Ng2</title>
     <base href="/">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta
+            name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <title>Fuse - Admin Theme</title>
+    <link
+            rel="stylesheet" href="css/vendor-3ad88f3d31.css">
+    <link rel="stylesheet" href="css/app-50c6015133.css">
+    <link
+            href="//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700italic,700,900,900italic"
+            rel="stylesheet" type="text/css">
 </head>
-<body>
-<app-root></app-root>
-<script type="text/javascript" src="/js/inline.bundle.js"></script><script type="text/javascript" src="/js/polyfills.bundle.js"></script><script type="text/javascript" src="/js/styles.bundle.js"></script><script type="text/javascript" src="/js/vendor.bundle.js"></script><script type="text/javascript" src="/js/main.bundle.js"></script></body>
+<!--[if lt IE 10]>
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
+<![endif]-->
+<body md-theme="@{{vm.themes.active.name}}" md-theme-watch ng-controller="IndexController as vm"
+      class="@{{state.current.bodyClass || ''}}">
+<ms-splash-screen id="splash-screen">
+    <div class="center">
+        <div class="logo">
+            <span>F</span></div>
+        <div class="spinner-wrapper">
+            <div class="spinner">
+                <div class="inner">
+                    <div class="gap"></div>
+                    <div
+                            class="left">
+                        <div class="half-circle"></div>
+                    </div>
+                    <div class="right">
+                        <div class="half-circle"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</ms-splash-screen>
+<div id="main" class="animate-slide-up" ui-view="main" layout="column"></div>
+<ms-theme-options></ms-theme-options>
+<script src="js/vendor-4674aa66b0.js"></script>
+<script
+        src="js/app-e84aa0411a.js"></script>
+</body>
 </html>
