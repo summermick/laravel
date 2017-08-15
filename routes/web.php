1a859ecbd6ee8af223c0e20153cdbc8e2f1a2a89
summermick <summermick@gmail.com>
2017-08-14 20:41:50 -0700

    AngularController added

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AngularController@serveApp');
+
+Route::get('/unsupported-browser', 'AngularController@unsupported');
